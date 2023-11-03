365a2fa7abb5651e27265a393b21a764a5cfedd3
dlwogur5234 <dlwogur5234@naver.com>
2023-11-03 10:02:59 +0900

    231103
    pt일지

---
+++ src/main/java/com/cloud/pt/ptLog/PtTypeController.java
@@ -0,0 +1,13 @@
+package com.cloud.pt.ptLog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PtTypeController {
+	
+	@Autowired
+	private PtTypeService ptTypeService;
+	
+	
+}
