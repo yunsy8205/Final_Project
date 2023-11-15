febc4b1feafe14d064065fe464aa7a1396c64574
dlwogur5234 <dlwogur5234@naver.com>
2023-11-15 10:09:22 +0900

    231115
    1

--- src/main/java/com/cloud/pt/ptLog/PtLogService.java
+++ src/main/java/com/cloud/pt/ptLog/PtLogService.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cloud.pt.member.MemVO;
+
 @Service
 public class PtLogService {
 	
@@ -22,6 +24,7 @@ public class PtLogService {
 	public int delLog(PtLogVO ptLogVO) throws Exception{
 		return ptLogDAO.delLog(ptLogVO);
 	}
+	
 }
 
 
